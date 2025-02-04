72b6c04487eeb6e25cd2714ad44dadd97f681b32
NestJul <nestjul@yandex.ru>
2025-02-04 19:34:16 +0700

    gradleTestNg

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldBeRemain0() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void shouldBeRemain900() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1100);
+        int expected = 900;
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void shouldBeRemain300() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(700);
+        int expected = 300;
+        assertEquals(actual,expected);
+    }
+}
